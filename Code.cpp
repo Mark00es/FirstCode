87be6bf9c9349e8ac70a7e4f9425e5c0f94fea3b
Marco <marco.escobar@ucb.edu.bo>
2022-02-06 17:34:47 -0400

    Primer menu

---
+++ Code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+void opciones() {
+    cout << "Welcome to Cato's Bank" << endl;
+    cout << "Choose an option" << endl;
+    cout << "1. Create new account" << endl;
+    cout << "2. Make a deposit" << endl;
+    cout << "3. Make a withdrawal" << endl;
+    cout << "4. Show all accounts" <<endl;
+    cout << "5. exit" << endl;
+}
+int main(){
+    opciones();
+}
